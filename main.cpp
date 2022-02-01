dd9f11a20543c56a610f58b84c5964d58e85f916
Alibaba1337 <98716025+Alibaba1337@users.noreply.github.com>
2022-02-01 23:10:45 +0100

    zmiana na mnozenie w metodzie police

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 int policz(int a, int b){
-    return a + b;
+    return a * b;// mnozenie
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "2 + 3 =" <<policz(2, 3) << std::endl;
+    std::cout << "2 + 3 = " <<policz(2, 3) << std::endl;
     return 0;
 }
